d7546ae45c1320ea7a084106c10eabedd7cf8e5b
Ralph Palomar <rpalomar@pepper.com.au>
2021-04-06 09:31:33 +0800

    experiment with twitter authorization

--- main.py
+++ main.py
@@ -90,20 +90,20 @@ def generate_twitter_auth_header():
         }
 
         # APPEND OTHER OAUTH ARGS
+        oauth_signature_data = oauth_headers.copy()
         for k, v in request.args.items():
             if k.startswith('oauth_'):
-                oauth_headers[k] = urllib.parse.quote(v, safe='')
+                oauth_signature_data[k] = urllib.parse.quote(v, safe='')
 
         # SORT BY HEADER KEY NAME
         output_string_array = []
-        for k, v in sorted(oauth_headers.items()):
+        for k, v in sorted(oauth_signature_data.items()):
             output_string_array.append(f'{k}%3D{v}')
 
         # CREATE A SIGNATURE BASE STRING AND GENERATE HMAC SHA1 SIGNATURE
         output_string = twitter_method + '&' + urllib.parse.quote(twitter_api, safe='') + '&' + '&'.join(output_string_array)
         log_payload("OUTPUT_STRING", output_string)
         signing_key = urllib.parse.quote(consumer_secret, safe='') + '&' + urllib.parse.quote(token_secret, safe='')
-        # log_payload("SIGNING KEY", signing_key)
         hmac_signature = base64.b64encode(hmac.new(bytes(signing_key, 'utf-8'), bytes(output_string, 'utf-8'), sha1).digest()).decode()
 
         # APPEND THE HMAC SHA1 SIGNATURE TO THE HEADERS
