0ae7f9d5f373b4df8f207ef52b862b5205dd2f80
Ralph Palomar <rpalomar@pepper.com.au>
2021-04-24 08:37:01 +0800

    started google authorization

--- applications/google.py
+++ applications/google.py
@@ -1,6 +1,5 @@
 from flask import make_response
 import config
-import requests
 import os
 
 
@@ -8,10 +7,11 @@ def authorize():
     try:
         response = make_response()
         response.headers['Location'] = f'https://accounts.google.com/o/oauth2/v2/auth?' \
-            f'client_id={os.environ["GOOGLE_CLIENT_ID"]}&' \
-            f'redirect_uri={os.environ["GOOGLE_CALLBACK_URL"]}&' \
-            f'response_type=code&' \
-            f'scope=https://www.googleapis.com/auth/spreadsheets'
+                                       f'client_id={os.environ["GOOGLE_CLIENT_ID"]}&' \
+                                       f'redirect_uri={os.environ["GOOGLE_CALLBACK_URL"]}&' \
+                                       f'response_type=code&' \
+                                       f'scope=https://www.googleapis.com/auth/spreadsheets&' \
+                                       f'access_type=offline'
 
         return response, 302
 
