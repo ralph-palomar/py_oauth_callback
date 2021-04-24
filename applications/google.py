d2f6d8fa6c1f4c12433c946508e9f43d9b8b11f6
Ralph Palomar <rpalomar@pepper.com.au>
2021-04-24 08:16:58 +0800

    started google authorization

--- applications/google.py
+++ applications/google.py
@@ -5,7 +5,7 @@ import os
 
 def authorize():
     try:
-        res = requests.request('GET', 'https://accounts.google.com/o/oauth2/v2/auth', parameters={
+        res = requests.request('GET', 'https://accounts.google.com/o/oauth2/v2/auth', params={
             "client_id": os.environ['GOOGLE_CLIENT_ID'],
             "redirect_uri": os.environ['GOOGLE_CALLBACK_URL'],
             "response_type": "code",
