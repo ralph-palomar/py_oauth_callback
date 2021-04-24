765c34d91c84033f173fb4c616f9e50d8ebecb3d
Ralph Palomar <rpalomar@pepper.com.au>
2021-04-24 09:35:36 +0800

    google get access token

--- applications/google.py
+++ applications/google.py
@@ -15,7 +15,7 @@ def authorize():
                                        f'response_type=code&' \
                                        f'scope=https://www.googleapis.com/auth/spreadsheets&' \
                                        f'access_type=offline&' \
-                                       f'prompt=consent'
+                                       f'prompt=consent&approval_prompt=force'
 
         return response, 302
 
