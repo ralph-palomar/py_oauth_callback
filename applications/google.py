3b7ad838f0c14463732cb76f6c66e7274e25b11f
Ralph Palomar <rpalomar@pepper.com.au>
2021-04-24 08:29:08 +0800

    started google authorization

--- applications/google.py
+++ applications/google.py
@@ -1,3 +1,4 @@
+from flask import make_response
 import config
 import requests
 import os
@@ -5,13 +6,14 @@ import os
 
 def authorize():
     try:
-        res = requests.request('GET', 'https://accounts.google.com/o/oauth2/v2/auth', params={
-            "client_id": os.environ['GOOGLE_CLIENT_ID'],
-            "redirect_uri": os.environ['GOOGLE_CALLBACK_URL'],
-            "response_type": "code",
-            "scope": "https://www.googleapis.com/auth/spreadsheets"
-        })
-        return res
+        response = make_response()
+        response.headers['Location'] = f'https://accounts.google.com/o/oauth2/v2/auth?' \
+            f'client_id={os.environ["GOOGLE_CLIENT_ID"]}&' \
+            f'redirect_uri={os.environ["GOOGLE_CALLBACK_URL"]}&' \
+            f'response_type=code&' \
+            f'scope=https://www.googleapis.com/auth/spreadsheets'
+
+        return response, 302
 
     except Exception as e:
         config.logger.exception(e)
