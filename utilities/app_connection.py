0141c76ed3ee6eae09c35da73a1d87eb8f2518d4
Ralph Palomar <rpalomar@pepper.com.au>
2021-04-30 00:56:24 +0800

    GET apps from db

--- utilities/app_connection.py
+++ utilities/app_connection.py
@@ -1,13 +1,18 @@
+import config
 from config import mongo_db_local
 
 
 def all_app_connections():
-    mongodb = mongo_db_local()
-    result = []
-    for item in mongodb['app_connections'].find():
-        result.append({
-            "connection_name": item['connection_name'],
-            "connection_type": item['connection_type']
-        })
+    try:
+        mongodb = mongo_db_local()
+        result = []
+        for item in mongodb['app_connections'].find():
+            result.append({
+                "connection_name": item['connection_name'],
+                "connection_type": item['connection_type']
+            })
 
-    return result, 200
+        return result, 200
+
+    except Exception as e:
+        config.logger(e)
